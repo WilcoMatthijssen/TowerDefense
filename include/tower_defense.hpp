694493d7c6638f162e719d74451ccf097eac7761
Wilco <57558724+WilcoMatthijssen@users.noreply.github.com>
2023-11-02 21:09:47 +0100

    Draw and delete bullets

--- include/tower_defense.hpp
+++ include/tower_defense.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#ifndef TD_TOWER_DEFENSE_HPP
+#define TD_TOWER_DEFENSE_HPP
 #include <SFML/Graphics.hpp>
 #include <vector>
 #include <tuple>
@@ -9,6 +11,7 @@
 #include <TD_tower.hpp>
 #include <TD_map.hpp>
 #include <TD_fileparser.hpp>
+#include <TD_bullet.hpp>
 
 
 namespace TD{
@@ -17,6 +20,7 @@ namespace TD{
         Tower m_tower;
         std::vector<Defender> m_defenders;
         std::vector<Attacker> m_attackers;
+        std::vector<Bullet> m_bullets;
 
         size_t m_current_wave_number;
         std::vector<std::tuple<std::string, size_t>> waves;
@@ -27,12 +31,16 @@ namespace TD{
             m_map  .draw(window);
             m_tower.draw(window);
 
-            for(const auto& attacker: m_attackers){
+            for(const Attacker& attacker: m_attackers){
                 attacker.draw(window);
             }
 
-             for(const auto& defender: m_defenders){
+            for(const Defender& defender: m_defenders){
                 defender.draw(window);
+            }
+
+            for(const Bullet& bullet: m_bullets){
+                bullet.draw(window);
             }
         }
 
@@ -42,21 +50,26 @@ namespace TD{
             }
 
             for(auto& defender: m_defenders){
-                defender.update(m_attackers);
+                defender.update(m_attackers, m_bullets);
             }
+
+            // Also directly removes the bullet from the vector when it hits an attacker
+            std::erase_if(m_bullets, [&](Bullet& bullet){
+                return bullet.update(m_attackers);
+            });
         }
+
+
 
         void attacker_spawn_routine(){
 
         }
         
         void start(){
-            m_defenders.resize(1);
+            m_defenders.push_back(Defender());
 
             m_attackers.resize(1);
-            m_defenders[0].set_target(&m_attackers[0]);
             m_attackers[0].configure({80,80});
-            // a.set_position({600,300});
             m_map.set_position_and_size({1,1}, {100,100});
             m_map.create_tiles({10,10});
 
@@ -76,9 +89,12 @@ namespace TD{
         }
 
         bool have_defenders_won(){
-            return (get_current_wave_number() == get_amount_of_waves()) && (get_remaining_attackers() == 0);
+            return (get_current_wave_number() == get_amount_of_waves()) && is_round_won();
         }
 
+        bool is_round_won(){
+            return m_attackers.empty();
+        }
         bool have_attackers_won(){
             return m_tower.is_destroyed();
         }
@@ -87,3 +103,5 @@ namespace TD{
         }
     };
 };
+
+#endif

9473a7eeb213b0469f5687168d8e871af0da4b1f
Wilco <57558724+WilcoMatthijssen@users.noreply.github.com>
2024-03-13 21:24:44 +0100

    Added placing of turrets and spawning of attackers

--- include/tower_defense.hpp
+++ include/tower_defense.hpp
@@ -6,27 +6,31 @@
 #include <tuple>
 #include <string>
 
-#include <TD_attacker.hpp>
-#include <TD_defender.hpp>
-#include <TD_tower.hpp>
-#include <TD_map.hpp>
-#include <TD_fileparser.hpp>
-#include <TD_bullet.hpp>
+#include "TD_attacker.hpp"
+#include "TD_defender.hpp"
+#include "TD_tower.hpp"
+#include "TD_map.hpp"
+#include "TD_fileparser.hpp"
+#include "TD_bullet.hpp"
 
 
 namespace TD{
     class Tower_defense{
+    private:
         Map m_map;
         Tower m_tower;
         std::vector<Defender> m_defenders;
         std::vector<Attacker> m_attackers;
         std::vector<Bullet> m_bullets;
+        Fileparser f;
+        size_t id_counter = 0;
+
 
         size_t m_current_wave_number;
         std::vector<std::tuple<std::string, size_t>> waves;
 
 
-    public:
+    public: 
         void draw(sf::RenderWindow& window) const{
             m_map  .draw(window);
             m_tower.draw(window);
@@ -43,37 +47,55 @@ namespace TD{
                 bullet.draw(window);
             }
         }
-
+        sf::Clock m_reload_clock;
         void update(){
-            for(auto& attacker: m_attackers){
+            for(Attacker& attacker: m_attackers){
                 attacker.update();
             }
 
-            for(auto& defender: m_defenders){
+            for(Defender& defender: m_defenders){
                 defender.update(m_attackers, m_bullets);
             }
 
+
+   
+
             // Also directly removes the bullet from the vector when it hits an attacker
-            std::erase_if(m_bullets, [&](Bullet& bullet){
-                return bullet.update(m_attackers);
-            });
+            std::erase_if(m_bullets, [&](Bullet& bullet){ return bullet.update(m_attackers); });
         }
 
-
-
         void attacker_spawn_routine(){
+         if(m_reload_clock.getElapsedTime().asSeconds() > .10f){
+                m_reload_clock.restart();
+                m_attackers.emplace_back();
+                m_attackers.back().configure({80,80}, f);
+
+                m_attackers.back().set_route(&(m_map.get_route()));
+            }
 
         }
         
         void start(){
-            m_defenders.push_back(Defender());
-
-            m_attackers.resize(1);
-            m_attackers[0].configure({80,80});
+            f.load();
             m_map.set_position_and_size({1,1}, {100,100});
-            m_map.create_tiles({10,10});
+            m_map.create_tiles(f);
+            //m_reload_clock.restart();
 
-            m_attackers[0].set_route(&(m_map.get_route()));
+        }
+
+        void place_turret(sf::Vector2f pos){
+            for(auto& def: m_defenders){
+                if(def.is_selected(pos)){
+                    // std::cout<<def.get_id() << std::endl;
+                }
+            }
+
+            const sf::Vector2i tile{static_cast<int>(pos.x) / 100, static_cast<int>(pos.y)/100};
+
+            if(m_map.reserve_tower_spot(tile)){
+                m_defenders.emplace_back(sf::Vector2f(tile.x * 100, tile.y * 100) + sf::Vector2f{50.0f, 50.0f}, id_counter++);
+            }
+
         }
         
         size_t get_current_wave_number() const{
@@ -92,12 +114,15 @@ namespace TD{
             return (get_current_wave_number() == get_amount_of_waves()) && is_round_won();
         }
 
+
         bool is_round_won(){
             return m_attackers.empty();
         }
+
         bool have_attackers_won(){
             return m_tower.is_destroyed();
         }
+
         bool is_game_over(){
             return have_attackers_won() || have_defenders_won();
         }

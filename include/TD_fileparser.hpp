0c813d071ec7bdbf8a20c5be440e2ea4998ca557
Wilco <57558724+WilcoMatthijssen@users.noreply.github.com>
2024-03-13 21:20:41 +0100

    Add json config

--- include/TD_fileparser.hpp
+++ include/TD_fileparser.hpp
@@ -3,25 +3,51 @@
 #include <SFML/Graphics.hpp>
 #include <string>
 #include <map>
+#include <fstream>
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <memory>
+using json = nlohmann::json;
 
 namespace TD{
 
-
     class Fileparser{
     public:
-        std::map<std::string, sf::Texture> texture;
+        void load(){
+            std::string file_path = "../assets/config.json";
 
-        sf::Vector2f get_tower_position(){
-            return {1,1};
-        }
+            try {
+                // Open the file
+                std::ifstream file_stream(file_path);
+                if (!file_stream.is_open()) {
+                    std::cerr << "Error opening file: " << file_path << std::endl;
+                    return;
+                }
 
-        sf::Texture & get_texture(std::string filename){
-            if(!texture.contains(filename)){
-                texture[filename].loadFromFile(filename);
+                // Parse the JSON file
+                file_stream >> json_data;
+
+                // Close the file
+                file_stream.close();
+            } catch (const std::exception& e) {
+                std::cerr << "Error parsing JSON: " << e.what() << std::endl;
             }
 
-            return texture.at(filename);
+            // std::cout << "Loaded JSON data:\n" << json_data.dump(4) << std::endl;
         }
-        
+
+
+        std::map<std::string, std::unique_ptr<sf::Texture>> texture;
+        json json_data;
+
+
+        sf::Texture * get_texture(std::string filename){
+            if(!texture.contains(filename)){
+                texture[filename] = std::make_unique<sf::Texture>();
+                bool success = texture[filename]->loadFromFile(filename);
+            }
+            return texture.at(filename).get();
+        }
+            
     };
 };
